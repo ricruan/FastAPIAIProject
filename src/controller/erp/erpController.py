c43604816e3f74781ec5dbfccc0b2c3f5a3f08c7
ricruan <ric@ruan.com>
2025-06-26 09:49:31 +0800

    执行SqlAP纳管

--- src/controller/erp/erpController.py
+++ src/controller/erp/erpController.py
@@ -1,9 +1,9 @@
 import json
-
 import requests
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
-
+from sqlmodel import Session
+from src.db.db import get_db
 from src.myHttp.bo.httpResponse import HttpResponse
 from src.myHttp.utils.myHttpUtils import normal_post
 from src.service.erpService import erp_execute_sql
@@ -17,11 +17,11 @@ class SQLQuery(BaseModel):
     sql: str
 
 @router.post("/execute-sql-query")
-async def execute_sql_query(sql: SQLQuery):
+async def execute_sql_query(sql: SQLQuery, db: Session = Depends(get_db)):
 
     try:
-        # 发送 POST 请求，设置超时时间为 10 秒
-        response = await erp_execute_sql(sql)
+        # 发送 POST 请求
+        response = await erp_execute_sql(sql,db)
 
         return HttpResponse.success(response)
 
--- src/service/erpService.py
+++ src/service/erpService.py
@@ -1,8 +1,16 @@
+import logging
+from fastapi import Depends
+from src.dao.apiInfoDao import get_info_by_api_code
+from src.db.db import get_db
 from src.myHttp.utils.myHttpUtils import normal_post
+from sqlmodel import Session
 
+logger = logging.getLogger(__name__)
 url = "https://pmserp.toasin.cn/api/demo/executeSqlQuery"
+ERP_EXEC_SQL_API_CODE = "erp_exec_sql"
 
-async def erp_execute_sql(sql):
+async def erp_execute_sql(sql, session: Session):
+    api_info = get_info_by_api_code(session,ERP_EXEC_SQL_API_CODE)
     if isinstance(sql,str):
         sql = {"sql": sql}
     else:
